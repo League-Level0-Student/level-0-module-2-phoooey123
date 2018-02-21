46f065f3ad361aae6097aee2ddf87791aebaf6ce
Ryder <league@wts22.attlocal.net>
2018-02-20 16:52:55 -0800

    Random and Method Calling.

--- src/methods/AnimalFarm.java
+++ src/methods/AnimalFarm.java
@@ -1,62 +1,59 @@
 package methods;
 
-//Copyright (c) The League of Amazing Programmers 2013-2017
-//Level 0
-
-
-import java.io.File;
-
-import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.swing.JOptionPane;
 
 public class AnimalFarm {
 
-AnimalFarm() {
-	/*
-	 * 1. Ask the user which animal they want, then play the sound of that
-	 * animal.
-	 */
-
-	/* 2. Make it so that the user can keep entering new animals. */
-}
-
-void playMoo() {
-	playNoise(mooFile);
-}
-
-void playQuack() {
-	playNoise(quackFile);
-}
-
-void playWoof() {
-	playNoise(woofFile);
-}
-
-String quackFile = "quack.wav";
-String mooFile = "moo.wav";
-String woofFile = "woof.wav";
-String meowFile = "meow.wav";
-String llamaFile = "llama.wav";
-
-
-/* Ignore this stuff */
-
-public void playNoise(String soundFile) {
-	try {
-		 Clip clip = AudioSystem.getClip();
-		 clip.open(AudioSystem.getAudioInputStream(getClass().getResource(soundFile)));
-		 clip.start();
-		 Thread.sleep(3400);
-	} catch (Exception ex) {
-  	ex.printStackTrace();
+	AnimalFarm() {
+		/*
+		 * 1. Ask the user which animal they want, then play the sound of that animal.
+		 */
+		String answer = JOptionPane.showInputDialog("What animal do you want to hear?");
+		if (answer.equals("dog")) {
+			playWoof();
+		} else if (answer.equals("cow")) {
+			playMoo();
+		} else if (answer.equals("duck")) {
+			playQuack();
+		}
+		/* 2. Make it so that the user can keep entering new animals. */
 	}
-}
 
-public static void main(String[] args) {
-	new AnimalFarm();
-}
+	void playMoo() {
+		playNoise(mooFile);
+	}
+
+	void playQuack() {
+		playNoise(quackFile);
+	}
+
+	void playWoof() {
+		playNoise(woofFile);
+	}
+
+	String quackFile = "quack.wav";
+	String mooFile = "moo.wav";
+	String woofFile = "woof.wav";
+	String meowFile = "meow.wav";
+	String llamaFile = "llama.wav";
+
+	/* Ignore this stuff */
+
+	public void playNoise(String soundFile) {
+		try {
+			Clip clip = AudioSystem.getClip();
+			clip.open(AudioSystem.getAudioInputStream(getClass().getResource(soundFile)));
+			clip.start();
+			Thread.sleep(3400);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		new AnimalFarm();
+	}
 
 }
-
--- src/random/IntroToRandom.java
+++ src/random/IntroToRandom.java
@@ -7,23 +7,24 @@ import java.util.Random;
 
 public class IntroToRandom {
 	public static void main(String[] args) {
-		
+
 		int r = 0;
-		
-		//3. Now make r random. 
-		//   Create an object of the Random class
-		//	 use .nextInt() to get a random number
-		
 
-		//4. Limit the random number between 0 and 100
+		// 3. Now make r random.
+		// Create an object of the Random class
+		// use .nextInt() to get a random number
+		Random random = new Random();
+		r = random.nextInt(311) - 222;
+		// 4. Limit the random number between 0 and 100
 
-		//5. Limit the random number between 25 and 75. Hint: ((highest value - lowest value)+1) + lowest value
+		// 5. Limit the random number between 25 and 75. Hint: ((highest value - lowest
+		// value)+1) + lowest value
 
-		//6. Challenge: Limit the random number between -222 and 88
+		// 6. Challenge: Limit the random number between -222 and 88
 
-		//1. Print out the value of r
-		
-		//2. Run the program.  What number appears in the console?
-		//   Run it again. Is the number the same?
+		// 1. Print out the value of r
+		System.out.println(r);
+		// 2. Run the program. What number appears in the console?
+		// Run it again. Is the number the same?
 	}
 }
